2cf653a2f9a854b488646a86492ce1fe466e19f8
Dimir <vladdou@yahoo.com>
2020-10-25 16:32:50 +0200

    initial commit

---
+++ blogtutor/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from blogtutor.users.models import User
+from blogtutor.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
